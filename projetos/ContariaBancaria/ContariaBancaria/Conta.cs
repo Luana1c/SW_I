a0c838717c8fb480f650d0fa4dae851608b0ef01
Luana Garcia <124801836+Luana1c@users.noreply.github.com>
2024-03-15 12:43:42 -0300

    pasta projeto- dia 15/03 foi conta bancaria

---
+++ projetos/ContariaBancaria/ContariaBancaria/Conta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContariaBancaria
+{
+    public class Conta
+    {
+        private string? Num_conta { get; set; }
+        private double Saldo { get; set; }
+        private double Limite { get; set; }
+
+        public void consuta_saldo()
+        {
+            Console.WriteLine("O seu saldo é de R$" + Saldo);
+        }
+        public void depositar(double valor) {
+            Saldo += valor;
+        }
+        public void sacar(double valor) {
+            if (valor < Saldo)
+            {
+                Saldo -= valor;
+            }
+            else
+            {
+                Console.WriteLine("SEU SALDO É INSULFICIENTE")
+            }
+         
+        
+        
+        }
+        public void ajuste_limite( double valor)
+        {
+            Limite = valor;
+        }
+   
+    }
+}
---
+++ projetos/ContariaBancaria/ContariaBancaria/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ContariaBancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao Banco da Luana!");
+
+            Conta conta = new Conta();
+            Console.WriteLine("CONSULTANDO O SALDO");
+            conta.consuta_saldo();
+            Console.WriteLine("deposito");
+            conta.depositar(1000.35);
+            Console.WriteLine("consulta o saldo!");
+            conta.consuta_saldo();
+           // Console.WriteLine("Sacar!");
+           //conta.sacar(500);
+           // Console.WriteLine("Consulta o saldo !");
+           //conta.consuta_saldo();
+           // Console.WriteLine("Saca o valor!");
+           // conta.sacar(1000);
+
+
+        }
+    }
+}
